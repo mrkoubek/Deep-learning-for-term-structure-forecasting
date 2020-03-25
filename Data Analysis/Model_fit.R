8186614421492d73e11c8f39a3d55b2c59caaaba
David <mrkoubek@gmail.com>
2020-03-25 22:37:24 +0100

    Models - added neural networks
    Basic neural network models added for our futures time series, without factor models.

--- Data Analysis/Model_fit.R
+++ Data Analysis/Model_fit.R
@@ -1,12 +1,20 @@
 # Master Thesis
 # Model fitting follows
-# v1.0 - first preprocessing split and graph for small data
+# v1.1 - basic NN models for our time series, without factor models
 
 rm(list = ls())
 
+# Bind R to a Python environment that has TensorFlow with GPU support
+# Do this first before loading any further packages, otherwise the R session can be binded with a default CPU environment and you will have to restart R to rebind
+	# library(reticulate)
+	# use_condaenv("r-tensorflow-gpu") # Specify the name of a conda environment.
+
 # Packages
 if (!require("pacman")) install.packages("pacman") # installs pacman package if not installed yet
+# tf_config()
 pacman::p_load(pacman, dplyr, tidyr, ggplot2, scales, Cairo, zoo, xtable, tibble) # load packages TBD e.g. these, edit when u determine which needed/used..
+p_load(forecast, reticulate, keras, tensorflow)
+tf_config()
 
 # Workspace
 setwd("E:/Google_Drive/Diploma_Thesis/Code")
@@ -14,14 +22,19 @@ setwd("E:/Google_Drive/Diploma_Thesis/Code")
 load(file = "Workspaces/Data_04_split-small_US_tick_05-2019.RData") # 0min
 print("Workspace rdy set go!")
 
+
+#############################
+###### Split datasets #######
+#############################
+
 # Picks the future
-futurenames
+futurenames # shall contain US and EU various maturities eventually
 future <- 1
 futurenames[future]
 
 # Train and test split
-# dataFutures_tmp <- data_small_aday$Close # one day of data
-dataFutures_tmp <- data_small_amonth$Close # one month of data
+# dataFutures_tmp <- data_small_aday$Close # one day of data, 24k obs
+dataFutures_tmp <- data_small_amonth$Close # one month of data, 780k obs
 (end <- length(dataFutures_tmp))
 (split_train <- round(3/5 * end))
 (split_val <- round(4/5 * end))
@@ -39,6 +52,11 @@ dataFutures_train <- diff(dataFutures_train)
 dataFutures_val <- diff(dataFutures_val)
 dataFutures_test <- diff(dataFutures_test)
 summary(dataFutures_train)
+
+
+#############################
+###### Graph datasets #######
+#############################
 
 # Graph of the split original dataset
 	data_df <- data_frame(time = 1:(length(dataFutures_train_orig) + length(dataFutures_val_orig) + length(dataFutures_test_orig)),
@@ -106,6 +124,223 @@ summary(dataFutures_train)
 
 
 
-########################## TRAINING ##########################
+#############################
+#### TBD Factor models ######
+#############################
+
+# Not programmed yet, shall run the data through the factor models (DNS) before training the NNsn.
+
+
+
+
+#############################
+########## ANNs #############
+#############################
+
 ls()
+
+### Prepare parameters ###
+
+# Windowing the data
+lstm_num_timesteps <- 4
+# lstm_num_timesteps <- 12
+
+window_data <- function(data) { # window the input data
+	t(sapply(1:(length(data) - lstm_num_timesteps), function(x) data[x:(x + lstm_num_timesteps - 1)]))
+}
+
+data_train <- window_data(dataFutures_train)
+data_val <- window_data(dataFutures_val)
+data_test <- window_data(dataFutures_test)
+data_train[1:5, 1:4]
+
+window_labels <- function(data) { # window the labels data
+	sapply((lstm_num_timesteps + 1):(length(data)), function(x) data[x])
+}
+
+labels_train <- window_labels(dataFutures_train)
+labels_val <- window_labels(dataFutures_val)
+labels_test <- window_labels(dataFutures_test)
+labels_train[1:5]
+
+
+# Keras LSTMs expect the input array to be shaped as (no. samples, no. time steps, no. features), yet we have just the two axes, need to add an axis "no.features" at the end that will just say that we have 1 feature (we have just one column/price to feed)
+# dim(data_train)
+# dim(data_val)
+# dim(data_test)
+# K <- backend() # don't need this approach anymore, from Keras 2.1.2 can use k_expand_dims and k_eval
+# data_train <- K$eval(K$expand_dims(data_train, axis = 2L)) # 2L because of this https://github.com/rstudio/tensorflow/issues/190
+data_train <- k_eval(k_expand_dims(data_train, axis = -1)) # -1 for the last axis to expand at
+data_val <- k_eval(k_expand_dims(data_val, axis = -1))
+data_test <- k_eval(k_expand_dims(data_test, axis = -1))
+dim(data_train)
+str(data_train)
+class(data_train)
+
+
+# LSTM input shape: (samples, time steps, features)
+num_samples <- dim(data_train)[1]
+num_steps <- dim(data_train)[2]
+num_features <- dim(data_train)[3]
+c(num_samples, num_steps, num_features)
+
+
+
+#############################
+####### Training NNs ########
+#############################
+
+# TensorBoard initialisation/stopping
+# dir.create("TensorBoard_logs")
+tensorboard(log_dir = "TensorBoard_logs/tmp", host = "127.0.0.1", port = "1001", launch_browser = utils::browseURL("http://127.0.0.1:1001"), reload_interval = 5) # Launch TensorBoard and wait for output in specified directory
+# tensorboard(log_dir = "TensorBoard_logs", host = "127.0.0.1", port = "1001", reload_interval = 5) # Launch TensorBoard manually at http://127.0.0.1:1001 and wait for output in specified directory
+tensorboard(log_dir = "TensorBoard_logs/tmp", action = "stop") # stops the TensorBoard
+# browseURL("http://127.0.0.1:1001") # launches the TensorBoard url
+
+##### TBD Tune #####
+# Define manually parameters here that will be used in the model
+### MAE ###
+	n_epochs <- 50 # for now 50 takes 10min for smallest network, later obviously need to train longer to optimise until it starts overfitting
+	batch <- 128
+
+	parameters_architecture <- list(nlayers = 1, units = c(8, 0, 0), dropout = 0.2, recurrent_dropout = 0.2) # 11s per epoch on a month of data
+	parameters_compile <- list(loss = "mae", optimizer = "rmsprop", metrics = "mse") ### for MAE ###
+	# parameters_compile <- list(loss = "mse", optimizer = "rmsprop", metrics = "mae") ### for MSE trials ###
+	parameters_fit <- list(epochs = n_epochs, batch_size = batch)
+	parameters_1 <- c(parameters_architecture, parameters_compile, parameters_fit)
+
+	parameters_architecture <- list(nlayers = 1, units = c(32, 0, 0), dropout = 0.2, recurrent_dropout = 0.2)
+	# parameters_fit <- list(epochs = n_epochs, batch_size = batch) # if we want to change sth
+	parameters_2 <- c(parameters_architecture, parameters_compile, parameters_fit)
+
+	parameters_architecture <- list(nlayers = 2, units = c(8, 16, 0), dropout = 0.1, recurrent_dropout = 0.5)
+	parameters_3 <- c(parameters_architecture, parameters_compile, parameters_fit)
+
+	parameters_architecture <- list(nlayers = 2, units = c(32, 64, 0), dropout = 0.1, recurrent_dropout = 0.5)
+	parameters_4 <- c(parameters_architecture, parameters_compile, parameters_fit)
+
+	parameters_architecture <- list(nlayers = 3, units = c(32, 64, 128), dropout = 0.1, recurrent_dropout = 0.5) # 43s per epoch
+	parameters_5 <- c(parameters_architecture, parameters_compile, parameters_fit)
+
+	parameters <- list(NN_1L_6N = parameters_1, NN_1L_32N = parameters_2,
+					   NN_2L_6N12N = parameters_3, NN_2L_32N64N = parameters_4,
+					   NN_3L_32N32N64N = parameters_5)
+	str(parameters)
+
+# Keras LSTM model
+# Looping the layers to automate it
+build_model_n <- function(nlayers = 1, units = rep(6, nlayers), dropout = 0, recurrent_dropout = 0,
+						  loss = "mae", optimizer = "rmsprop", metrics = "mse", ...) {
+	k_clear_session() # clearing the model/session first with backend to avoid TensorBoard errors
+	
+	model <- keras_model_sequential()
+	for (i in 1:nlayers) { # hidden layers
+		model %>% layer_lstm(units = units[i], return_sequences = (i != nlayers), dropout = dropout, recurrent_dropout = recurrent_dropout, input_shape = c(num_steps, num_features))
+	}
+	model %>% layer_dense(units = 1) # output layer
+
+	model %>% compile(
+		loss = loss,
+		optimizer = optimizer,
+		metrics = metrics
+	)
+}
+
+save <- function(what, name) {
+	save_model_hdf5(what, paste0("Models/tmp/Model_", name, ".h5"))
+	save.image(file = paste0("Workspaces/tmp/Image_", name, "_trained.RData"))
+}
+
+
+model_names <- c()
+len <- 1
+# for (i in 1:len) {
+	i <- 1 # number of the run
+	print(paste("Iteration", i, "/", len))
+	for (hyp_tune in 1:3) { # 1:5 or 1:(length(parameters)
+		# hyp_tune <- 5 # in case u want to skip the for loop and manually select which one to train
+		print(paste("Hyperparameter tuning", hyp_tune, "/", length(parameters)))
+
+		model <- do.call(what = build_model_n, args = parameters[[hyp_tune]])
+		print(model)
+
+		layers_name <- c()
+		for (j in 1:parameters[[hyp_tune]]$nlayers) {layers_name <- paste0(layers_name, model$layers[[j]]$units, "N-")}
+		model_name <- paste0(futurenames[future], "-1H-2003-2017-LSTM-k", lstm_num_timesteps, "-", length(model$layers) - 1, "L-", layers_name,
+							  100 * model$layers[[1]]$dropout, "pDr-", 100 * model$layers[[1]]$recurrent_dropout, "pRDr-",
+							  toupper(model$loss),"-", parameters_compile$optimizer, "-", toupper(model$metrics_names[2]),"-", parameters[[hyp_tune]]$epochs, "E_run_", i)
+		print(paste("Training model", model_name))
+
+		# dir.create("TensorBoard_logs", showWarnings = FALSE)
+		log_run_dir <- paste0("TensorBoard_logs/tmp/", model_name)
+		model_checkpoint_dir <- paste0("Models/checkpoints/", model_name)
+		dir.create(model_checkpoint_dir, showWarnings = FALSE, recursive = TRUE)
+		model_checkpoint_name <- "/{epoch:02d}E-{val_loss:.5f}vloss.hdf5"
+
+		callbacks_list = list(
+			callback_tensorboard(
+				log_dir = log_run_dir,
+				histogram_freq = 1 # Records activation histograms every 1 epoch
+				# embeddings_freq = 1 # Records embedding data every 1 epoch
+			),
+			# callback_early_stopping(
+			# 	monitor = "val_loss",
+			# 	patience = 100
+			# ),
+			callback_model_checkpoint(
+				filepath = paste0(model_checkpoint_dir, "/", model_checkpoint_name),
+				monitor = "val_loss"
+				# save_best_only = TRUE # uncomment if u want to checkpoint only the best models, but when it's commented out, all models will be written (provided the model_checkpoint_dir name for each model varies w/ next epoch..)
+			)
+		)
+
+		history <- model %>% fit(
+			data_train, labels_train,
+			epochs = parameters[[hyp_tune]]$epochs, batch_size = parameters[[hyp_tune]]$batch_size, # batch_size = 128, epochs = 500 for real run; epochs = i*ep for increasing number of epochs w/ iterations
+			validation_data = list(data_val, labels_val), # computes loss and accuracy for validation data after every epoch
+			# validation_split = 0.2,
+			callbacks = callbacks_list, # for TensorBoard logging
+			# initial_epoch = 100, # TBD try, integer, Epoch at which to start training (useful for resuming a previous training run).
+			verbose = 2
+		)
+
+		# dir.create("Workspaces/runs", showWarnings = FALSE, recursive = TRUE)
+		save(what = model, name = model_name)
+	}
+# }
+
+plot(history)
+
+
+### Evaluation ###
+
+
+
+
+
+
+
+
+
+
+#####################
+####### Notes #######
+#####################
+
+# TBD:
+	# factor models
+	# evaluation
+	# extensive hyperpar tuning once the code is scalable
+
+#####################
+#### Keras setup ####
+#####################
+
+	### Set up the environment once: ###
+	# For CPU it easier as follows, for GPU follow variou online documentation. Still, it best to set up the environment in conda first for both versions.
+	# Install Rtools for building packages outside CRAN, Anaconda (it's a "Python manager") and then install Keras from R:
+		# install.packages("devtools")
+		# devtools::install_github("rstudio/keras") # install keras R package (not the actual "Keras" yet, just the "keras" R package that can later install "Keras" and "TensorFlow"!)
+		# library(keras) # load in the keras package
+		# install_keras() # installs default Keras library and TensorFlow backend
 
