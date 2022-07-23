325070170d306eb66b6ad61b8cecd626358f47eb
David <mrkoubek@gmail.com>
2022-07-23 17:32:39 +0200

    Preprocessing - all maturities cleaned
    Had to rewrite cleaning into the loading up process, so it's smaller straight away. With the unclean version I couldn't clean it due to it being already 25GB and any operation on it was unsuccessful (datetime edits etc). Now we have all maturities in one object and they're already cleaned up.

--- Data Analysis/Data_conversion.R
+++ Data Analysis/Data_conversion.R
@@ -6,14 +6,16 @@ rm(list = ls())
 
 # Packages
 if (!require("pacman")) install.packages("pacman") # installs pacman package if not installed yet
-pacman::p_load(pacman, ggplot2, zoo, xtable, tibble, data.table) # load packages TBD e.g. these, edit when u determine which needed/used..
+pacman::p_load(pacman, ggplot2, zoo, xtable, tibble, data.table, stringr, lubridate) # load packages TBD e.g. these, edit when u determine which needed/used..
 
 # Workspace
 setwd("E:/Google_Drive/Diploma_Thesis/Code")
 # Loading basic functions
 load(file = "Workspaces/Basics_01_functions.RData") # we load the basic functions first, then in addition latest ones below:
 # Loading data
-load(file = "Workspaces/Data_01_loaded_TUFVTYUS.RData") # takes 8min!! we load the latest version of a prepared workspace
+# load(file = "Workspaces/tmp_Data_01_loaded_test.RData") # takes 8min!! we load the latest version of a prepared workspace
+# load(file = "Workspaces/Data_01_loaded_TUFVTYUS.RData") # takes 8min!! we load the latest version of a prepared workspace
+
 # load(file = "Workspaces/Data_02_cleaned_US.RData") # we load the latest version of a prepared workspace
 # load(file = "Workspaces/Data_03_trimmed-small_US.RData") # we load the latest version of a prepared workspace
 print("Workspace rdy set go!")
@@ -91,8 +93,60 @@ memory.limit(size = 60000)
 # dataFutures[[3]] <- fread(filePaths[[3]], select = columns_to_select, header = TRUE, data.table = TRUE) # takes Xmin
 # time_end(startTime)
 
+
+# Benchmarking the speed of joining two columns
+	# data <- dataFutures[[1]]
+
+	# p_load(microbenchmark)
+	# p_load(stringi)
+	# p_load(stringr)
+	# b <- microbenchmark(
+	# 	stringrS = stringr::str_c(data$Date, data$Time, sep = " "),
+	# 	paste = paste(data$Date, data$Time),
+	# 	paste0 = paste0(data$Date, " ", data$Time),
+	# 	# stringi = stringi::stri_c(data$Date, " ", data$Time),
+	# 	# stringiS = stringi::stri_c(data$Date, data$Time, sep = " "),
+	# 	# stringr = stringr::str_c(data$Date, " ", data$Time),
+	# 	unite = data %>% unite("DateTime", c("Date", "Time")),
+	# 	times = 10
+	# )
+	# b
+		 # Unit: seconds
+		 #     expr       min        lq      mean    median       uq      max neval
+		 # stringrS  7.181248  7.305488  8.816205  7.404083  7.48882 14.95292    10
+		 #    paste 10.318972 10.366211 11.114709 10.430571 10.76827 16.64542    10
+		 #   paste0 12.285284 12.385603 14.121135 12.461027 13.16945 20.57462    10
+		 #    unite 10.309580 10.462405 15.038606 14.380144 18.89820 22.98747    10
+
+# Cleans the data so it takes less space
+clean_data <- function(data) {
+	# Set to show milliseconds
+	op_backup <- options("digits.secs") # set seconds digits to have milliseconds, by default digits.secs = 0, save orig options, TBD
+	options(digits.secs = 3) # set seconds digits to have milliseconds, by default digits.secs = 0, save orig options, TBD
+
+	data$Time <- stringr::str_c(data$Date, data$Time, sep = " ") # combine Date and Time columns, stringr is the fastes from our testing above, since we're joining characters
+	data[, c("Date")] <- NULL # getting rid of redundant columns
+
+
+	# data.table does not support POSIXlt (which is result of only the strptime()) data types for performance reason, use POSIXct or ITime
+	# https://stackoverflow.com/questions/21487614/error-creating-r-data-table-with-date-time-posixlt
+	# data$Date <- as.POSIXct(strptime(data$Date, format = "%m/%d/%Y %H:%M:%OS", tz = "GMT")) # 1min
+	# Improving upon strptime, which is the main bottleneck here, we use the lubridate's fast_strptime, about 3x faster!
+	data$Time <- as.POSIXct(fast_strptime(data$Time, format = "%m/%d/%Y %H:%M:%OS", tz = "GMT")) # 1min
+
+	# Have a look at the data
+	print(str(data))
+	print(head(data))
+	print(tail(data))
+	# summary(data$Close)
+	options(op_backup) # reset options, only after the above as.character is performed, else it writes the data_head w/o milliseconds
+
+	return(data)
+}
+
+
 # Load up the datasets
-for (i in 1:dim) {
+for (i in 1:1) {
 	startTime <- time_start() # time the execution
 	print(paste("Started loading", filePaths[[i]]))
 
@@ -102,7 +156,60 @@ for (i in 1:dim) {
 	size_objects("dataFutures")
 }
 
-size_objects() # check how large is the dataFutures vector, 25Gb w all 4 maturities
+
+# Load up the datasets - NEW function w cleaning
+# TU 2min, FV 7min, TY 21min, US 7min
+for (i in 1:dim) {
+	startTime <- time_start() # time the execution
+	print(paste("Started loading", filePaths[[i]]))
+
+	dataFutures[[i]] <- fread(filePaths[[i]], select = columns_to_select, header = TRUE, data.table = TRUE) # main load, takes a few mins
+	
+		print("Size before cleaning")		
+		size_objects("dataFutures")
+
+		print("Cleaning the dataset")
+		dataFutures[[i]] <- clean_data(dataFutures[[i]])
+
+		print("Size after cleaning")
+		size_objects("dataFutures") # TU 2.3 -> 1.3, FV 6.2 -> 3.5, TY 9.8 -> 5.9, US 6.3 -> 3.6
+
+	# Save the workspace of the maturity
+	print("Saving the workspace")
+	save.image(file = paste0("Workspaces/Data_01_loaded_cleaned_", futurenames[i], ".RData"))
+
+	# reinstate the empty variable, so it fast for each maturity
+	dataFutures <- array(list(NULL), dim = dim, dimnames = list(futurenames)) # an array in which we'll save all our data
+
+	time_end(startTime)
+}
+
+
+# Merge the workspaces
+maturities <- c("TU", "FV", "TY", "US")
+data <- array(list(NULL), dim = dim, dimnames = list(futurenames)) # an array in which we'll merge our data
+
+for (i in 1:dim) {
+	print(paste("Loading workspace for", maturities[i]))
+	load(file = paste0("Workspaces/Data_01_loaded_cleaned_", maturities[i], ".RData")) # we load the basic functions first, then in addition latest ones below:
+	data[[i]] <- dataFutures[[i]]
+	size_objects("data")
+	rm(dataFutures)
+	if(i == dim) print("Done merging all maturities.")
+}
+
+str(data)
+size_objects("data") # the vector of all cleaned maturities is 15Gb (before cleaning it was 25Gb)
+
+# Save the workspace of all maturities in one variable
+# print("Saving the workspace")
+# save.image(file = paste0("Workspaces/Data_01_loaded_cleaned_TUFVTYUS.RData"))
+
+
+
+
+
+
 
 # Save the env
 ifelse(!dir.exists("Workspaces"), dir.create("Workspaces"), "Directory already exists.") # creates a "Workspaces" directory if it doesn't exist yet
@@ -110,6 +217,23 @@ startTime <- time_start()
 # save.image(file = "Workspaces/Data_01_loaded_TUFVTYUS.RData") # save the workspace, takes 8min
 time_end(startTime)
 
+
+
+
+
+		print(paste("Loading up dataset", i, "/", dim, futurenames[i]))		
+		dataFutures[i] <- load_RData_dataFutures(paste0("Workspaces/Data_01_loaded_", futurenames[i], ".RData"))[i]
+
+		print(paste("Size before cleaning", futurenames[i]))		
+		size_objects("dataFutures")
+
+		print(paste("Cleaning the dataset", futurenames[i]))
+		dataFutures[[i]] <- clean_data(dataFutures[[i]])
+
+		print(paste("Size after cleaning", futurenames[i]))
+		size_objects("dataFutures")
+
+		print(paste("Dataset", i, "/", dim, futurenames[i], "loaded up."))
 
 
 ################### TBD old loading code from here
@@ -174,11 +298,37 @@ time_end(startTime)
 	  get(ls()[ls() == "dataFutures"])
 	}
 
+
+
+		# testclean <- dataFutures[[2]]
+
+		print(paste("Size before cleaning", "testclean"))		
+		size_objects("testclean")
+
+		print(paste("Cleaning the dataset", "testclean"))
+		testclean <- clean_data(dataFutures[[2]])
+
+		print(paste("Size after cleaning", "testclean"))
+		size_objects("testclean")
+
+
+
+
+
 	dataFutures <- array(list(NULL), dim = dim, dimnames = list(futurenames)) # an array in which we'll save all our data
 	for (i in 1:3) {
-		print(paste("Loading up dataset", i, "/", dim, futurenames[i]))
+		print(paste("Loading up dataset", i, "/", dim, futurenames[i]))		
 		dataFutures[i] <- load_RData_dataFutures(paste0("Workspaces/Data_01_loaded_", futurenames[i], ".RData"))[i]
+
+		print(paste("Size before cleaning", futurenames[i]))		
 		size_objects("dataFutures")
+
+		print(paste("Cleaning the dataset", futurenames[i]))
+		dataFutures[[i]] <- clean_data(dataFutures[[i]])
+
+		print(paste("Size after cleaning", futurenames[i]))
+		size_objects("dataFutures")
+
 		print(paste("Dataset", i, "/", dim, futurenames[i], "loaded up."))
 	}
 
@@ -209,13 +359,12 @@ time_end(startTime)
 #########################
 
 # Statistics
-str(data_US)
-(data_US_str <- capture.output(str(data_US)))
-head(data_US)
-tail(data_US)
-data_US[1000, ]
-(data_US_summary <- summary(data_US))
-data_US_head <- head(data_US)[, -ncol(data_US)]
+str(dataFutures$US)
+(data_US_str <- capture.output(str(dataFutures$US)))
+(data_US_head <- head(dataFutures$US))
+tail(dataFutures$US)
+dataFutures$US[1000, ]
+(data_US_summary <- summary(dataFutures$US))
 
 print(xtable(data_US_summary, caption = "US 30-Year bond summary statistics"),
 	type = "latex", file = paste0("Results/Tables/data_US_summary.tex"), include.rownames = FALSE)
@@ -225,16 +374,84 @@ print(xtable(data_US_head, caption = "US 30-Year bond data glimpse"),
 
 rm(data_US_str, data_US_summary, data_US_head)
 
+
+
 # Cleaning
-data <- data_US[, c("Date", "Time", "Close")]
-data$Date <- paste(data$Date, data$Time) # combine Date and Time columns
-data$Date <- strptime(data$Date, format = "%m/%d/%Y %H:%M:%OS", tz = "GMT")
+
+# TMP testing
+	data <- dataFutures$TU[, c("Date", "Time", "Close")]
+	data$Date <- paste(data$Date, data$Time) # combine Date and Time columns
+	test <- data[1:13, ]
+	rm(dataFutures)
+	# The following workspace image takes 1min to save and is 150MB
+	# save.image(file = "Workspaces/tmp_Data_01_loaded_test.RData") # save the workspace
+
+	test[, c("Time")] <- NULL
+	class(test$Date)
+	strip <- strptime(test$Date, format = "%m/%d/%Y %H:%M:%OS", tz = "GMT")
+	class(strip)
+	str(strip)
+	typeof(strip)
+	typeof(strip[[1]])
+	strip[[1]]
+
+	strip_POSIXct <- as.POSIXct(strip)
+	class(strip_POSIXct)
+	str(strip_POSIXct)
+	typeof(strip_POSIXct)
+
+	test$Date <- strip
+	test$new <- rep(8, 8)
+	test$Date <- strip_POSIXct
+	test
+
+	df <- data.frame(test)
+	df
+	str(df)
+	class(df)
+	typeof(df)
+	df$Date <- strip
+	df
+# TMP end testing
+
+# Doesn't work for the larger maturities, cannot do the as.POSIXct, no RAM room to work with, gotta do it one by one in
+# the previous 01 - load chapter, while loading up each maturity straight away clean it
+# TBD cleaning the datetime columns saves so much space GB, maybe I could do that for all and keep all the other columns we couldn't load up (like High and Low), but if we don't need them w/e
+# TBD test with one US maturity, then rewrite to run on all 5 lists of dataFutures
+size_objects("dataFutures") # 25Gb
+# data.table does not support POSIXlt (which is result of only the strptime()) data types for performance reason, use POSIXct or ITime
+# https://stackoverflow.com/questions/21487614/error-creating-r-data-table-with-date-time-posixlt
+dataFutures$TU$Date <- as.POSIXct(strptime(dataFutures$TU$Date, format = "%m/%d/%Y %H:%M:%OS", tz = "GMT")) # 5min
+size_objects("dataFutures") # 2Gb
+op_backup <- options(digits.secs = 3) # set seconds digits, by default digits.secs = 0, save orig options, TBD
+tail(dataFutures$US) # prints with milliseconds
+dataFutures$TU[, c("Time")] <- NULL # getting rid of redundant columns
+size_objects("dataFutures") # 1Gb
+str(dataFutures$US)
+head(dataFutures$US)
+summary(dataFutures$US$Close)
+(dataFuturesUS_str <- str(dataFutures$US))
+(dataFuturesUS_summary <- summary(dataFutures$US))
+dataFuturesUS_str <- glimpse(dataFutures$US)
+dataFuturesUS_head <- head(dataFutures$US)
+dataFuturesUS_head$Date <- as.character(dataFuturesUS_head$Date) # in order for xtable to work, it doesn't work with date classes
+options(op_backup) # reset options, only after the above as.character is performed, else it writes the data_head w/o milliseconds
+
+
+
+
+size_objects("data") # 3Gb
+# data.table does not support POSIXlt (which is result of only the strptime()) data types for performance reason, use POSIXct or ITime
+# https://stackoverflow.com/questions/21487614/error-creating-r-data-table-with-date-time-posixlt
+data$Date <- as.POSIXct(strptime(data$Date, format = "%m/%d/%Y %H:%M:%OS", tz = "GMT")) # 1min
+size_objects("data") # 2Gb
 op_backup <- options(digits.secs = 3) # set seconds digits, by default digits.secs = 0, save orig options, TBD
 tail(data) # prints with milliseconds
 data[, c("Time")] <- NULL # getting rid of redundant columns
+size_objects("data") # 1Gb
 str(data)
 head(data)
-summary(data)
+summary(data$Close)
 (data_str <- str(data))
 (data_summary <- summary(data))
 data_str <- glimpse(data)
@@ -260,6 +477,9 @@ rm(data_summary, data_str, data_head)
 ### 03 - Trim data ######
 #########################
 
+# TBD CONT
+load(file = "Workspaces/Data_01_loaded_cleaned_TUFVTYUS.RData") # we load all the maturities
+
 # Take an excerpt from the data to easily work on/visualise, e.g. only the day 20th May 2019 or the month May 2019:
 data_small_aday <- subset(data, Date >= as.POSIXlt("2019-05-20", tz = "GMT") & Date < as.POSIXlt("2019-05-21", tz = "GMT"))
 data_small_amonth <- subset(data, Date >= as.POSIXlt("2019-05-01", tz = "GMT") & Date < as.POSIXlt("2019-06-01", tz = "GMT"))
@@ -281,6 +501,11 @@ summary(data_small)
 # Save the workspace
 # The following takes 0min to save and is 6MB
 save.image(file = "Workspaces/Data_03_trimmed-small_US.RData")
+
+
+#############################
+### 04 - Aggregate data #####
+#############################
 
 
 
@@ -339,6 +564,35 @@ dates <- c("02/27/92", "02/27/92", "01/14/92", "02/28/92", "02/01/92")
 times <- c("23:03:20", "22:29:56", "01:03:30", "18:21:03", "16:56:26")
 x <- paste(dates, times)
 strptime(x, "%m/%d/%y %H:%M:%S")
+
+
+
+# R is smart so reassigning a variable doesn't create a copy, https://stackoverflow.com/questions/22951811/how-to-rename-a-variable-in-r-without-copying-the-object
+tracemem(dataFutures[[4]])
+tracemem(data[[4]])
+
+
+# Profiling
+p_load(profr)
+x <- profr(example(glm))
+plot(x)
+
+y <- profr(
+	clean_data(dataFutures[[1]])
+	)
+plot(y)
+
+Rprof(tmp <- tempfile())
+example(glm)
+Rprof()
+summaryRprof(tmp)
+
+Rprof(tmp <- tempfile())
+	clean_data(dataFutures[[1]])
+Rprof()
+summaryRprof(tmp)
+
+
 
 
 
